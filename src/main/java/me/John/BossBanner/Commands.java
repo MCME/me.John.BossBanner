06b92dc25803f78b93809dbe979e7c1e7370f74d
IoannisKl <klironomos.john@gmail.com>
2020-08-29 20:59:33 +0300

    Tried to use brigadier command parser, but I can't get the imports to work correctly

---
+++ src/main/java/me/John/BossBanner/Commands.java
@@ -0,0 +1,12 @@
+/*package me.John.BossBanner;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.CommandSource;
+
+public final class Commands {
+    private Commands() {}
+
+    public static void register(CommandDispatcher<CommandSource> dispatcher){
+        dispathcer.register();
+    }
+}*/
